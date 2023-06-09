5fb77c151aaa8e635d4688f4c5446bab497c274e
egreich <egr65@nau.edu>
2023-06-09 14:13:47 +0200

    Update file structure

---
+++ scripts/01_filter_data.R
@@ -0,0 +1,24 @@
+### Script to format data in a consistent way with README files
+### input data: Qian's collected data
+### output data: csv dataframe for each site with consistent units
+
+### ORDER:
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
